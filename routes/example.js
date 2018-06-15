00d5563f8cecb367fcff87457d1fb3e8c023c9dc
Yan de Paula <yanw100@gmail.com>
2018-06-15 18:45:34 -0300

    Adicionado script de start com nodemon. -Adicionado gitignore.

--- routes/example.js
+++ routes/example.js
@@ -3,10 +3,10 @@ var router = express.Router()
 
 // define the about route
 router.get('/', function (req, res) {
-    res.send('This should go to index of Example controller')
+    res.send('This should redirect to index of Example controller')
 })
 router.get('/about', function (req, res) {
-    res.send('About Example')
+    res.send('This shold return the About about Example')
 })
 
 module.exports = router
